08a4634ae4408e93b85cd2869d71d03253d5e31c
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2024-01-08 23:21:06 +0630

    feat: add another phoye

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" Sai Hlyan Phyoe "}
+            By{" Sai Hlyan Phyoe Phyoe"}
             <img
               src="/vercel.svg"
               alt="Vercel Logo"
